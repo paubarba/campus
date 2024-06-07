e43b015d5697882c75a50604a3537fdece2aa2c7
paubarba <0224604@up.edu.mx>
2024-06-07 16:21:39 -0600

    first commit

--- attendance/attendance.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path("attendance/", include("attendance.urls")),
-    path("admin/", admin.site.urls),
-]
--- campus/urls.py
+++ campus/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import render
 
 urlpatterns = [
+    path('', lambda request: render(request, "home.html")),
     path('admin/', admin.site.urls),
     path("attendance/", include("attendance.urls")),
+    path("quizzes/", include("quizzes.urls")),
 ]
-
